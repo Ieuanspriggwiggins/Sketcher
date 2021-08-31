c9d895d3ae92e23b7c6c8c620a2c9d5ebb55cbdb
Ieuan Sprigg-Wiggins <ieuanspriggwiggins@gmail.com>
2021-08-31 15:40:49 +0100

    Implement basic colouring system and clearing and toggling grid

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //Reference to container for the grid
 const container = document.getElementById("main-grid");
 
+let value = 16
+
 //Creates the grid for the sketching area
 function makeRows(rows, cols){
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
     for(c = 0; c < (rows * cols); c++){
         let cell = document.createElement("div");
+        cell.addEventListener("mouseover", changeColour);
         container.appendChild(cell).className = "grid-item";
     };
 };
@@ -17,10 +20,11 @@ let displayGridSlider = document.getElementById("display-grid-size");
 
 //Sets the default grid when page opened
 setDefaultGrid();
+document.getElementById("main-grid").classList.toggle("main-grid-toggle");
 
 //Changes the grid size
 function changeGridSize() {
-    let value = sizeSlider.value;
+    value = sizeSlider.value;
     container.innerHTML = "";
     makeRows(value, value);
     displayGridSlider.innerHTML = value.toString() + "x" + value.toString();
@@ -28,8 +32,31 @@ function changeGridSize() {
 
 //Set default grid
 function setDefaultGrid() {
-    let value = 16;
     container.innerHTML = "";
     makeRows(value, value);
     displayGridSlider.innerHTML = value.toString() + "x" + value.toString();   
+}
+
+//Current colouring colour
+let currentColour = "#1E2019";
+
+//Reference to grid controls
+normalColourBtn = document.getElementById("default-color-btn");
+rainbowModeBtn = document.getElementById("rainbow-mode-btn");
+clearBtn = document.getElementById("clear-btn");
+removeGridBtn = document.getElementById("remove-grid-btn");
+
+//Event listeners for buttons
+normalColourBtn.addEventListener("click", function() {
+    currentColur = "#1E2019"
+});
+clearBtn.addEventListener("click", function() {
+    changeGridSize();
+})
+removeGridBtn.addEventListener("click", function() {
+    document.getElementById("main-grid").classList.toggle("main-grid-toggle");
+})
+
+function changeColour(e) {
+    e.target.style.backgroundColor = currentColour;
 }
